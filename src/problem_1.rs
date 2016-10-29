ab45da625d511c0da042a7b86e7b970d3ba28ad3
H4rtland <h4rtland@gmail.com>
2016-10-29 15:12:00 +0100

    fixed spacing in problem 1

--- src/problem_1.rs
+++ src/problem_1.rs
@@ -1,7 +1,7 @@
 pub fn problem_1() {
     let mut sum = 0;
     for x in 1..1000 {
-        if x % 3 ==0 || x % 5 == 0 {
+        if x % 3 == 0 || x % 5 == 0 {
             sum += x;
         }
     }

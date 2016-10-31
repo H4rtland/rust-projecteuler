963938eaed9c959ffce07a7c34f3d88b27277538
H4rtland <h4rtland@gmail.com>
2016-10-31 00:13:10 +0000

    generalised problem 15

--- src/problem_15.rs
+++ src/problem_15.rs
@@ -1,23 +1,28 @@
 pub fn problem_15() {
     let mut grid = Vec::new();
 
-    // 21 because 20 squares in grid but the last square has an extra vertex
-    for _ in 0..21 {
+    let width = 20;
+    let height = 20;
+
+    // width+1 because width squares in grid but the last square has an extra vertex
+    for _ in 0..width+1 {
         let v: Vec<i64>;
-        v = vec![0; 21];
+        v = vec![0; height+1];
         grid.push(v);
     }
 
-    for i in 0..21 {
+    for i in 0..width+1 {
         grid[i][0] = 1;
-        grid[0][i] = 1;
+    }
+    for j in 0..height+1 {
+        grid[0][j] = 1;
     }
 
-    for i in 1..21 {
-        for j in 1..21 {
+    for i in 1..width+1 {
+        for j in 1..height+1 {
             grid[i][j] = grid[i-1][j] + grid[i][j-1];
         }
     }
 
-    println!("Total paths through grid: {}", grid[20][20]);
+    println!("Total paths through grid: {}", grid[width][height]);
 }

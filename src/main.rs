3044aaf8ee0d9b19af41111ae49b9da4f871c116
H4rtland <h4rtland@gmail.com>
2016-11-01 22:22:09 +0000

    completed problem 18

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate time;
 use time::PreciseTime;
 
-mod problem_17;
+mod problem_18;
 
 fn main() {
     let start_time = PreciseTime::now();
-    problem_17::problem_17();
+    problem_18::problem_18();
     let end_time = PreciseTime::now();
     println!("Execution took {} seconds", start_time.to(end_time))
 }

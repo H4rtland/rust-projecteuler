743f36219d6d97f5e072968c89326eca8ab74504
H4rtland <h4rtland@gmail.com>
2016-11-02 16:23:46 +0000

    completed problem 20

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate time;
 use time::PreciseTime;
 
-mod problem_19;
+mod problem_20;
 
 fn main() {
     let start_time = PreciseTime::now();
-    problem_19::problem_19();
+    problem_20::problem_20();
     let end_time = PreciseTime::now();
     println!("Execution took {} seconds", start_time.to(end_time))
 }
---
+++ src/problem_20.rs
@@ -0,0 +1,24 @@
+extern crate num;
+use self::num::bigint;
+use self::num::bigint::ToBigInt;
+
+fn factorial(n: i64) -> i64 {
+    if n == 1 {
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+
+pub fn problem_20() {
+    let mut big_number: bigint::BigInt = 1.to_bigint().unwrap();
+    for i in 1..101 {
+        big_number = big_number.checked_mul(&i.to_bigint().unwrap()).unwrap();
+    }
+    let string_representation: String = big_number.to_str_radix(10_u32);
+    let mut sum_of_digits = 0;
+    for c in string_representation.chars() {
+        sum_of_digits += c as i32 - 48;
+    }
+
+    println!("Sum of digits: {}", sum_of_digits);
+}
